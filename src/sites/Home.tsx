70be4759124dc4cbcf43e28e991f425f5d82c81f
Steppzilla <45403455+Steppzilla@users.noreply.github.com>
2022-11-02 18:07:19 +0100

    bugfix: print view

--- src/sites/Home.tsx
+++ src/sites/Home.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
     return (
         <>
-            <div className={"buttonRow menu"}>
+            <div className={"buttonRow menu noprint"}>
                 <button className={"noprint"}
                         onClick={handleFormatChange}>Format ändern
                 </button>
